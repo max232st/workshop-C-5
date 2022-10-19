c59f0d22f3c0c781e7c4806234d99e3f4593c1eb
max232st <max232st@gmail.com>
2022-10-19 21:32:55 +0300

    Семинар 5 task006_dz

---
+++ task006_dz/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Console.Clear();
+Console.WriteLine("Введите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+int[] array = new int[num];
+FillArray(array);
+NumArray(array);
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 999);
+        Console.Write($"{arr[i]} ");
+    }
+}
+void NumArray(int[] arr)
+{
+    int num = 0;
+    foreach (int el in arr)
+    {
+        if (el % 2 == 0)
+        {
+            num++;
+        }
+    }
+    Console.Write($"-> {num}");
+}
